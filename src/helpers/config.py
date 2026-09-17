8b5a925f4b1da6ffa7b5f57069151b732460f69c
ahmedzakaria75 <223326702+ahmedzakaria75@users.noreply.github.com>
2026-09-18 01:14:45 +0300

    refactor: enhance configuration management by integrating Pydantic settings

---
+++ src/helpers/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+
+        
+        APP_NAME:str
+        APP_VERSION:str
+
+        model_config = SettingsConfigDict(env_file=".env")
+
+
+def get_settings():
+    return Settings()
